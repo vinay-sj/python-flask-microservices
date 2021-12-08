6cbd102b83c4c0721295059087ef64ea8b9339dd
Archanajith <46067531+Archanajith@users.noreply.github.com>
2021-12-08 01:52:50 -0500

    Update docker-compose-init.tf

--- deployment_infrastructure/docker-compose-init.tf
+++ deployment_infrastructure/docker-compose-init.tf
@@ -110,8 +110,7 @@ resource "aws_instance" "deployment" {
 	yes | sudo docker exec -it cuser-service flask db init
 	yes | sudo docker exec -it cuser-service flask db migrate
 	yes | sudo docker exec -it cuser-service flask db upgrade
-	yes | sudo curl -i -d "name=prod1&slug=prod1&image=product1.jpg&price=100" -X POST localhost:5002/api/product/create
-        yes | sudo curl -i -d "name=prod2&slug=prod2&image=product2.jpg&price=200" -X POST localhost:5002/api/product/create
+	
 	EOF
 	]
 
