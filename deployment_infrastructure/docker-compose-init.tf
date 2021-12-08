812f7cdc803c96729e041ef10ede1fb305962bcb
bhosaleanagha <55074591+bhosaleanagha@users.noreply.github.com>
2021-12-08 03:21:05 -0500

    Update docker-compose-init.tf

--- deployment_infrastructure/docker-compose-init.tf
+++ deployment_infrastructure/docker-compose-init.tf
@@ -98,7 +98,7 @@ resource "aws_instance" "deployment" {
 
 	yes | sudo chmod +x /usr/local/bin/docker-compose
 	# yes | sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-	yes | sudo docker-compose -f ~/docker-compose.yml up -d
+	yes | sudo docker-compose -f ../docker-compose.yml up -d
 	yes | sudo docker exec -it corder-service flask db init
 	yes | sudo docker exec -it corder-service flask db migrate
 	yes | sudo docker exec -it corder-service flask db upgrade
